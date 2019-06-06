7538cb4cdf83d6c24be341fcc87a6b7dc42cfed5
sharolin <sharolin33@gmail.com>
2019-06-05 19:19:24 -0600

    ARITMETICAS

--- src/ast/expresiones/operacion/Aritmetica.java
+++ src/ast/expresiones/operacion/Aritmetica.java
@@ -13,7 +13,7 @@ import ast.expresiones.Expresion;
  *
  * @author sharolin
  */
-public class Aritmetica extends Operacion implements Expresion{
+public class Aritmetica extends Operacion implements Expresion {
 
     public Aritmetica(Expresion exp1, Expresion exp2, Operador op, int line, int col, Simbolo.Tipo tipo) {
         super(exp1, exp2, op, line, col, tipo);
@@ -22,22 +22,105 @@ public class Aritmetica extends Operacion implements Expresion{
     public Aritmetica(Expresion exp1, Operador op, int line, int col, Simbolo.Tipo tipo) {
         super(exp1, op, line, col, tipo);
     }
-    
-    
 
     @Override
     public Object getValue(Entorno lista) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Object op1 = exp1.getValue(lista);
+        Object op2 = exp2.getValue(lista);
+
+        if (op1 != null && op2 != null) {
+            tipo = tipoResultante(exp1, exp2, lista);
+
+            switch (op) {
+                case MAS:
+                    switch (tipo) {
+                        case STRING:
+                            return String.valueOf(exp1.getValue(lista)) + " " + String.valueOf(exp1.getValue(lista));
+
+                        case INT:
+                            return Integer.parseInt(String.valueOf(exp1.getValue(lista))) + Integer.parseInt(String.valueOf(exp2.getValue(lista)));
+
+                        case DOUBLE:
+                            return Double.parseDouble(String.valueOf(exp1.getValue(lista))) + Double.parseDouble(String.valueOf(exp2.getValue(lista)));
+
+                        default:
+                            System.out.println("Error de tipos para SUMA");
+                    }
+                    break;
+
+                case MENOS:
+                    switch (tipo) {
+
+                        case INT:
+                            return Integer.parseInt(String.valueOf(exp1.getValue(lista))) - Integer.parseInt(String.valueOf(exp2.getValue(lista)));
+
+                        case DOUBLE:
+                            return Double.parseDouble(String.valueOf(exp1.getValue(lista))) - Double.parseDouble(String.valueOf(exp2.getValue(lista)));
+
+                        default:
+                            System.out.println("Error de tipos para MENOS");
+                    }
+                    break;
+
+                case POR:
+                    switch (tipo) {
+                        case INT:
+                            return Integer.parseInt(String.valueOf(exp1.getValue(lista))) * Integer.parseInt(String.valueOf(exp2.getValue(lista)));
+
+                        case DOUBLE:
+                            return Double.parseDouble(String.valueOf(exp1.getValue(lista))) * Double.parseDouble(String.valueOf(exp2.getValue(lista)));
+
+                        default:
+                            System.out.println("Error de tipos para MULTIPLICACION");
+                    }
+                    break;
+
+                case DIVIDIR:
+                    switch (tipo) {
+                        case DOUBLE:
+                            if (Double.parseDouble(String.valueOf(exp2.getValue(lista))) > 0) {
+                                return Double.parseDouble(String.valueOf(exp1.getValue(lista))) / Double.parseDouble(String.valueOf(exp2.getValue(lista)));
+                            } else {
+                                System.out.println("Error division entre cero");
+                            }
+
+                        case INT:
+                            if (Double.parseDouble(String.valueOf(exp2.getValue(lista))) > 0) {
+                                return Integer.parseInt(String.valueOf(exp1.getValue(lista))) / Integer.parseInt(String.valueOf(exp2.getValue(lista)));
+                            } else {
+                                System.out.println("Error division entre cero");
+                            }
+                    }
+                    break;
+
+                case POTENCIA:
+                    switch (tipo) {
+                        case INT:
+                            return Math.pow(Double.parseDouble(String.valueOf(exp1.getValue(lista))), Double.parseDouble(String.valueOf(exp2.getValue(lista))));
+
+                        case DOUBLE:
+                            return Double.parseDouble(String.valueOf(exp1.getValue(lista))) * Double.parseDouble(String.valueOf(exp2.getValue(lista)));
+
+                        default:
+                            System.out.println("Error de tipos para POTENCIA");
+                    }
+                    break;
+            }
+            
+            return null;
+        }
+
+        return null;
     }
 
     @Override
     public Object getType(Entorno lista) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return tipoResultante(exp1, exp2, lista);
     }
 
     @Override
     public int getLine() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return line;
     }
-    
+
 }
