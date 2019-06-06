553b7642a0bcc947d5e0233ec637192a80f78488
sharolin <sharolin33@gmail.com>
2019-06-05 23:55:26 -0600

    asignacion

--- src/ast/instrucciones/Asignacion.java
+++ src/ast/instrucciones/Asignacion.java
@@ -6,24 +6,43 @@
 package ast.instrucciones;
 
 import ast.entorno.Entorno;
+import ast.entorno.Simbolo;
+import ast.expresiones.Expresion;
 
 /**
  *
  * @author sharolin
  */
-public class Asignacion implements Instruccion{
-    
-    
-    
+public class Asignacion implements Instruccion {
+
+    public String id;
+    public Expresion valor;
+    public int linea;
+    public int columna;
+
+    public Asignacion(String id, Expresion valor, int linea, int columna) {
+        this.id = id;
+        this.valor = valor;
+        this.linea = linea;
+        this.columna = columna;
+    }
 
     @Override
     public Object ejecutar(Entorno lista) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        Simbolo variable = lista.get(id, lista);
+        if (variable != null) {
+            if (variable.getTipo() == valor.getType(lista)) {
+                variable.setValor(valor.getValue(lista));
+            }
+        }
+
+        return null;
     }
 
     @Override
     public int getLine() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return linea;
     }
-    
+
 }
